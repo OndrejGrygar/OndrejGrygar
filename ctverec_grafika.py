550319174dbf6b1424721eabf1762ef837bfe5cb
OndrejGrygar <61903826+OndrejGrygar@users.noreply.github.com>
2020-04-11 16:58:57 +0200

    Add files via upload

---
+++ ctverec_grafika.py
@@ -0,0 +1,14 @@
+import turtle
+
+square = turtle.Turtle()
+square.forward(100)
+square.left(90)
+square.forward(100)
+square.left(90)
+square.forward(100)
+square.left(90)
+square.forward(100)
+
+
+
+turtle.done()
